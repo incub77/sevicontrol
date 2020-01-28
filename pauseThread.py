9860ec9ef4569a9d0469bf09aa1d410f877ce477
incub <incub@gmx.net>
2020-01-28 18:58:58 +0100

    added exeption

--- pauseThread.py
+++ pauseThread.py
@@ -15,22 +15,24 @@ class PauseThread(Thread):
         self.log = logging.getLogger("Pause")
         self.log.setLevel(logging.DEBUG)
 
-    def sleep(self, duration):
+    def pause(self, duration):
+        self.log.info("Going to sleep till %s" % time.strftime("%H:%M", time.localtime(time.time() + duration)))
         with self.lock:
-            self.log.info("Going to sleep till %s" % time.strftime("%H:%m", time.localtime(time.time() + duration)))
             self.wakeup_time = time.time() + duration
-            #TODO: try
+        try:
             self.cmd_queue.put(Modes.OFF)
+        except Full:
+            self.log.critical("Command queue full")
 
     def run(self) -> None:
         while 1:
             time.sleep(60)
-            if time.time() >= self.wakeup_time:
+            if self.wakeup_time and time.time() >= self.wakeup_time:
+                self.log.info("Waking up from sleep mode")
                 with self.lock:
                     self.wakeup_time = None
-                    try:
-                        self.cmd_queue.put(Modes.ON)
-                        self.log.info("Waking up from sleep mode")
-                    except Full:
-                        self.log.critical("Command queue full")
+                try:
+                    self.cmd_queue.put(Modes.ON)
+                except Full:
+                    self.log.critical("Command queue full")
 
